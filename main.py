0b01c02093d65cef7b1e1e048d45d9fc66c97ae2
itscd2 <113126803+itscd2@users.noreply.github.com>
2022-09-22 10:53:27 +0800

    update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from recommend import get_recom_result
 from manip_data import get_tech_token_by_id
 
 def run_recommend():
-    tfidf_fp = pickle.load(open('./semi_data/recom_dict_20220908-231214.pkl', 'rb'))
+    tfidf_fp = pickle.load(open('./semi_data/recom_dict_20220922-103812.pkl', 'rb'))
 
     #輸入要待推薦的文本 須為段好的詞 string
     #target_str 會放user的技術足跡,
@@ -12,10 +12,10 @@ def run_recommend():
 
     target_str= '專利 技術 監測 系統 智慧照顧 醫療人員 專利 人員 醫療'
 
-    target_str_2=get_tech_token_by_id('90')
+    target_str_2=get_tech_token_by_id('5100')
 
     #要排除的 tech id list
-    exclude_ids=['90']
+    exclude_ids=['5100']
 
     out_1 = get_recom_result(tfidf_fp, target_str, 5)
     out_2 = get_recom_result(tfidf_fp, target_str_2, 5, exclude_ids)
